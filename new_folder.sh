e9da330b02d7846ea22ff8b018f1f8e857ccd005
Julie He <juhe@ucdavis.edu>
2018-03-26 15:48:36 -0700

    Finalized new_folder.sh and upload_spectra.sh scripts

--- new_folder.sh
+++ new_folder.sh
@@ -1,28 +1,87 @@
 #!/bin/bash
-# Based on Steven's upload_to_box.sh script
-# This script will create new folders
+# This script creates new folders. Based on Steven's upload_to_box.sh
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: ./new_folder.sh BLAH BLAH"
-  exit 1
-fi
-
-# Make sure that current access token is up-to-date
+# Make sure that current access token is up-to-date; always needed?
 ./get_new_access_token.sh
 
+# reads access token in txt file and globally (g) removes quotes
 ACCESS_TOKEN=$( cat box_access_token.txt | sed 's/\"//g' )
-BOX_PATH="$1"
-FILE="$2"
-FOLDER_ID=0
+
+PARENT_FOLDER="material_compatibility"
+INPUT_FILE="../ANNIE/Gd_testing/file_ext.txt"
+#FOLDER_ID=0
+#NEXT_FOLDER_ID=$FOLDER_ID
 LIMIT=100
 OFFSET=0
-NEXT_FOLDER_ID=$FOLDER_ID
 HEADER="Authorization: Bearer $ACCESS_TOKEN"
 
-# pseudo code
-for $MATERIAL_FOLDER in $MATERIAL_COMPATIBILITY
-  check if imgs, spc, txt folders exist
-  if not then: 
-    create imgs folder
-    create spc folder
-    create txt folder
+echo "parent folder: $PARENT_FOLDER"
+echo "input file path: $INPUT_FILE"
+
+while read FILE_EXT; do
+#	echo "$FILE_EXT"
+
+	FOLDER_ID=0
+	NEXT_FOLDER_ID=$FOLDER_ID
+
+	IFS='/' read -a ARRAY <<< "$PARENT_FOLDER/$FILE_EXT"
+
+	for ELEMENT in "${ARRAY[@]}"; do
+		OFFSET=0
+		while :
+		do
+			RESPONSE=`curl "https://api.box.com/2.0/folders/${FOLDER_ID}/items?limit=${LIMIT}&offset=${OFFSET}" -H "$HEADER" -s`
+
+			ENTRIES=`echo $RESPONSE | ./JSON.sh | egrep '\[\"entries"\"\]' | cut -f2`
+
+			if [ "$ENTRIES" = "[]" ]; then
+				ERROR="true"
+				echo "  $ELEMENT folder not found"
+				break
+			else
+				NEXT_FOLDER_ID=`echo $RESPONSE | ./JSON.sh | egrep -A 4 "\[\"entries\",[0-9]*,\"type\"\][[:space:]]*\"folder\"" | egrep -B 3 "\[\"entries\",[0-9]*,\"name\"][[:space:]]*\"${ELEMENT}\"" | head -1 | cut -d "\"" -f6`
+
+				echo "$NEXT_FOLDER_ID" | grep '[a-zA-Z]' > /dev/null
+				VALID_FOLDER_ID=$?
+
+				if [ "$NEXT_FOLDER_ID" != "" ] && [ $VALID_FOLDER_ID = 1 ]; then
+					FOLDER_ID=$NEXT_FOLDER_ID
+					echo "  $ELEMENT folder id: $FOLDER_ID"
+					break
+				else
+					OFFSET=$((OFFSET+$LIMIT))
+				fi
+			fi
+		done
+	done
+
+	if [ "$ERROR" != "true" ]; then
+		# had issues with -d option, var expansion and quotes
+#		echo "$FOLDER_ID"
+
+		IMG={\"name\":\"img\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$IMG" -X POST 
+
+		# script still runs even if folder already exists
+#		STATUS1=`curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$IMG" -X POST -i -s | grep HTTP/1.1 | tail -1 | awk {'print $2'}` > /dev/null
+#		if [ $STATUS1 != 409 ]; then
+#			ERROR="true"
+#		else
+#			echo "created img folder"
+#		fi
+
+		SPC={\"name\":\"spc\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$SPC" -X POST
+
+		TXT={\"name\":\"txt\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$TXT" -X POST
+
+	fi
+
+	if [ "$ERROR" = "true" ]
+	then
+		echo "failed to create folders"
+		exit 1
+	fi
+
+done < "$INPUT_FILE"
--- upload_spectra.sh
+++ upload_spectra.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 # Uploads all the .spc and .txt files to box for each material
 
-for $MATERIAL in ${LIST_OF_FILE_EXT}
-  check all *_test/ folders in Gd_testing/
-  list each $MATERIAL out (all dates)
-  separate into .spc and .txt lists
-  upload to respective folders
+ANNIE_PATH="$HOME/ANNIE/Gd_testing"
+INPUT_FILE="file_ext.txt"
+PARENT_FOLDER="material_compatibility"
+
+while read FILE_EXT; do
+	# txt files
+	for txt in `find "$ANNIE_PATH"/*_test -type f -name "*[0-9]_$FILE_EXT.txt" -newermt '14 sep 2017'`; do
+#		echo "$PARENT_FOLDER/$FILE_EXT/txt"
+		./upload_to_box.sh "$PARENT_FOLDER/$FILE_EXT/txt" $txt
+	done
+
+	# spc files
+	for spc in `find "$ANNIE_PATH"/*_test -type f -name "*[0-9]_$FILE_EXT.spc" -newermt '14 sep 2017'`; do
+#		echo "$PARENT_FOLDER/$FILE_EXT/spc"
+		./upload_to_box.sh "$PARENT_FOLDER/$FILE_EXT/spc" $spc
+	done
+
+done < "$ANNIE_PATH/$INPUT_FILE"
