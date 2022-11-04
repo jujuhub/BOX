f99b9cd872205c330c134cd6e602b24b5b8bc60b
Julie He <juhe@ucdavis.edu>
2022-11-03 19:40:25 -0700

    regular updates

--- new_folder.sh
+++ new_folder.sh
@@ -7,8 +7,8 @@
 # reads access token in txt file and globally (g) removes quotes
 ACCESS_TOKEN=$( cat box_access_token.txt | sed 's/\"//g' )
 
-PARENT_FOLDER="material_compatibility"
-INPUT_FILE="../ANNIE/Gd_testing/file_ext.txt"
+PARENT_FOLDER="material_compatibility/before_soln_replacement"
+INPUT_FILE="../BOX/folder_list.txt" #"../ANNIE/Gd_testing/file_ext.txt"
 #FOLDER_ID=0
 #NEXT_FOLDER_ID=$FOLDER_ID
 LIMIT=100
@@ -24,7 +24,7 @@ while read FILE_EXT; do
 	FOLDER_ID=0
 	NEXT_FOLDER_ID=$FOLDER_ID
 
-	IFS='/' read -a ARRAY <<< "$PARENT_FOLDER/$FILE_EXT"
+	IFS='/' read -a ARRAY <<< "$PARENT_FOLDER" #/$FILE_EXT" # adjust parent
 
 	for ELEMENT in "${ARRAY[@]}"; do
 		OFFSET=0
@@ -55,12 +55,20 @@ while read FILE_EXT; do
 		done
 	done
 
+	# make new folders
 	if [ "$ERROR" != "true" ]; then
 		# had issues with -d option, var expansion and quotes
 #		echo "$FOLDER_ID"
+#		echo "$FILE_EXT"
 
-		IMG={\"name\":\"img\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
-		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$IMG" -X POST 
+		FOL={\"name\":\"$FILE_EXT\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$FOL" -X POST 
+
+#		MAT={\"name\":\"$FILE_EXT\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+#		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$MAT" -X POST 
+
+#		IMG={\"name\":\"img\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+#		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$IMG" -X POST 
 
 		# script still runs even if folder already exists
 #		STATUS1=`curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$IMG" -X POST -i -s | grep HTTP/1.1 | tail -1 | awk {'print $2'}` > /dev/null
@@ -70,11 +78,11 @@ while read FILE_EXT; do
 #			echo "created img folder"
 #		fi
 
-		SPC={\"name\":\"spc\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
-		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$SPC" -X POST
+#		SPC={\"name\":\"spc\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+#		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$SPC" -X POST
 
-		TXT={\"name\":\"txt\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
-		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$TXT" -X POST
+#		TXT={\"name\":\"txt\",\"parent\":{\"id\":\""$FOLDER_ID"\"}}
+#		curl "https://api.box.com/2.0/folders" -H "$HEADER" -d "$TXT" -X POST
 
 	fi
 
